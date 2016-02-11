eb2bbabd48fffaf5358bcb50a89dd5f213ea4d51
Damien Fourure <damien.fourure@gmail.com>
2016-02-11 14:59:12 +0100

    Initial Commit

---
+++ moteur_graphique/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  moteur_graphique
+//
+//  Created by Fourure Damien on 11/02/2016.
+//  Copyright © 2016 Fourure Damien. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
